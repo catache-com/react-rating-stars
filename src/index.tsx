61c7a039b51cd43f7786009f386ef3d3f6a84408
stephen <u6269649@anu.edu.au>
2023-07-29 16:23:16 +1000

    MAINT

--- src/index.tsx
+++ src/index.tsx
@@ -3,4 +3,4 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<div>olala</div>, document.getElementById("root"));
+ReactDOM.render(<div>react-rating-stars</div>, document.getElementById("root"));
