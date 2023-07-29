3e4b665518c3bfebd89f4e60c93d000c81a1cf06
stephen <u6269649@anu.edu.au>
2023-07-29 16:18:53 +1000

    INIT

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(<div>olala</div>, document.getElementById("root"));
